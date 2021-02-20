43088df8899ea6bcecfb7aee2d91fe0b03aa74ed
ashishsurya126 <ashishsurya126@gmail.com>
2021-02-21 00:25:05 +0530

    final commmit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,35 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [people, setPeople] = useState([]);
+  useEffect(() => {
+    fetch('/time')
+      .then((res) => res.json())
+      .then((data) => {
+        setPeople(data.items);
+      });
+  }, []);
+  console.log(people);
   return (
     <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-        <p>the current time is 0</p>
-      </header>
+      {people.map((person) => {
+        return (
+          <article
+            style={{
+              border: '1px solid black',
+              background: 'gray',
+              width: '90%',
+              margin: 'auto',
+            }}
+            key={person.id}
+          >
+            <h4>First Name : {person.first_name}</h4>
+            <h4>Second Name : {person.last_name}</h4>
+            <h5>email : {person.email}</h5>
+            <h5>Gender : {person.gender}</h5>
+          </article>
+        );
+      })}
     </div>
   );
 }
